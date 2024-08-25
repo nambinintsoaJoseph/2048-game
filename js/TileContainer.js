0af9110c951a09b33aeacf567e67c2d0c950beb2
RAZANAKANAMBININTSOA Joseph <raznambinintsoa3@gmail.com>
2024-08-25 14:51:39 +0200

    TileContainer : displayState() -> display the status in the user interface

--- js/TileContainer.js
+++ js/TileContainer.js
@@ -10,11 +10,11 @@ class TileContainer {
 
         // The position of the tiles must be different : 
         do {
-            position1.line = Math.floor(Math.random() * 3); 
-            position1.column = Math.floor(Math.random() * 3);
-            position2.line = Math.floor(Math.random() * 3); 
-            position2.column = Math.floor(Math.random() * 3);
-        } while(position1.line == position2.line && position1.column == position2.column)
+            position1.line = Math.floor(Math.random() * 4); 
+            position1.column = Math.floor(Math.random() * 4);
+            position2.line = Math.floor(Math.random() * 4); 
+            position2.column = Math.floor(Math.random() * 4);
+        } while(position1.line == position2.line && position1.column == position2.column);
 
         this.state[position1.line][position1.column]  = tileValue; 
         this.state[position2.line][position2.column] = tileValue; 
@@ -24,15 +24,43 @@ class TileContainer {
         const state = this.state;
         let numberEmptyBox = 0;
 
-        for (let i = 0; i < 4; i++) {
-            for (let j = 0; j < 4; j++) {
-                if (state[i][j] == 0) {
-                    numberEmptyBox++;
+        state.forEach(line => {
+            line.forEach(tileValue => {
+                if(tileValue == 0) {
+                    numberEmptyBox++
                 }
-            }
-        }
+            })
+        })
 
         return numberEmptyBox;
+    }
+
+    displayState() {
+        const state = this.state; 
+        const colors = {
+            tile2: '#00C855', 
+            tile4: '#D50000',
+            tile8: '#C51162', 
+            tile16: '#303F9F', 
+            tile32: '#444e4c', 
+            tile64: '#558B2F', 
+            tile128: '#BF360C',
+            tile256: '#372723', 
+            tile512: '#245A64',
+            tile1024: '#311B92', 
+            tile2048: '#1B5E20'
+        }; 
+
+        for(let i = 0; i < 4; i++) {
+            for(let j = 0; j < 4; j++) {
+                let currentTile = document.querySelector('.p' + i + j)
+                
+                if(state[i][j]) {
+                    currentTile.textContent = state[i][j]; 
+                    currentTile.style.backgroundColor = colors['tile' + state[i][j]]; 
+                }
+            }
+        }   
     }
 }
 

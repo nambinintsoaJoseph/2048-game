05e595f639e8ef4ee9880d99ead0d7447fb1cfc8
RAZANAKANAMBININTSOA Joseph <raznambinintsoa3@gmail.com>
2024-08-26 11:22:50 +0200

    (TileContainer.js) Implementation of methods for verifying authorized displacement

--- js/TileContainer.js
+++ js/TileContainer.js
@@ -16,7 +16,7 @@ class TileContainer {
             position2.column = Math.floor(Math.random() * 4);
         } while(position1.line == position2.line && position1.column == position2.column);
 
-        this.state[position1.line][position1.column]  = tileValue; 
+        this.state[position1.line][position1.column] = tileValue; 
         this.state[position2.line][position2.column] = tileValue; 
     }
 
@@ -62,7 +62,65 @@ class TileContainer {
             }
         }   
     }
+
+    checkWin() {
+        return this.state.some(currentTile => currentTile.includes(2048)); 
+    }
+
+    canMoveLeft() {
+        const state = this.state; 
+        for(let i = 0; i < 4; i++) {
+            for(let j = 1; j < 4; j++) {
+                if(state[i][j] !== 0) {
+                    // Check if the adjacent box is empty or has the same value 
+                    if(state[i][j-1] === 0 || (state[i][j-1] === state[i][j])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+
+    canMoveRight() {
+        const state = this.state; 
+        for(let i = 0; i < 4; i++) {
+            for(let j = 0; j < 3; j++) {
+                if(state[i][j] !== 0) {
+                    if(state[i][j+1] === 0 || (state[i][j+1] === state[i][j])) {
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+
+    canMoveUp() {
+        const state = this.state; 
+        for(let i = 1; i < 4; i++) {
+            for(let j = 0; j < 4; j++) {
+                if(state[i][j] !== 0) {
+                    if(state[i-1][j] === 0 || (state[i-1][j] === state[i][j])) {
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+
+    canMoveDown() {
+        const state = this.state; 
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 4; j++) {
+                if(state[i][j] !== 0) {
+                    if(state[i+1][j] === 0 || (state[i+1][j] === state[i][j])) {
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false; 
+    }
 }
-
-
-
